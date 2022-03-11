4becb61f0c447ce6727d371e312a3828815cb7e9
Damian Klein <klein.damian45@gmail.com>
2022-03-11 16:42:07 +0100

    Lab2 implementation

--- src/pl/gdynia/amw/oop/Main.java
+++ src/pl/gdynia/amw/oop/Main.java
@@ -1,8 +1,12 @@
 package pl.gdynia.amw.oop;
 
+import pl.gdynia.amw.oop.lab2.Lab2;
+
 public class Main {
 
-    public static void main(String args[]) {
-        System.out.println("test");
+    public static void main(String args []) {
+        var lab = new Lab2();
+        lab.run();
     }
+
 }
---
+++ src/pl/gdynia/amw/oop/lab2/Lab2.java
@@ -0,0 +1,9 @@
+package pl.gdynia.amw.oop.lab2;
+
+public class Lab2 {
+
+    public void run(){
+        System.out.println("Lab2 by Damian Klein");
+    }
+
+}
