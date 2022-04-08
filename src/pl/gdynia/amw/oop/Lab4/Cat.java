4ec447756da36b21d43a427858a45e42d5f65cf3
Damian Klein <klein.damian45@gmail.com>
2022-04-08 17:11:43 +0200

    Lab5 implementation

--- src/pl/gdynia/amw/oop/Lab4/Cat.java
+++ src/pl/gdynia/amw/oop/Lab4/Cat.java
@@ -1,13 +1,24 @@
 package pl.gdynia.amw.oop.Lab4;
 
-public class Cat extends Animal {
+public class Cat implements Animal {
+
+    private String name;
 
     public Cat(String name) {
-        super(name);
+        this.name = name;
     }
 
     @Override
     public void voice() {
         System.out.println("miau miau");
+    }
+
+    @Override
+    public void printName() {
+        System.out.printf("Name of an Animal is: %s%n", name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/pl/gdynia/amw/oop/Lab4/Dog.java
+++ src/pl/gdynia/amw/oop/Lab4/Dog.java
@@ -1,13 +1,24 @@
 package pl.gdynia.amw.oop.Lab4;
 
-public class Dog extends Animal {
+public class Dog implements Animal {
+
+    private String name;
 
     public Dog(String name) {
-        super(name);
+        this.name = name;
     }
 
     @Override
     public void voice() {
         System.out.println("hau hau");
+    }
+
+    @Override
+    public void printName() {
+        System.out.printf("Name of an Animal is: %s%n", name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
